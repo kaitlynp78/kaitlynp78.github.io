96bc7a300e721d0210e35146d4e811132a9368c5
kaitlynp78 <kspinkston05@gmail.com>
2025-02-06 23:24:32 -0500

    JavaScript Intro Pt. 2

--- csce242/exercises/04.js.intro/script.js
+++ csce242/exercises/04.js.intro/script.js
@@ -11,3 +11,29 @@ document.getElementById("btn-color").onclick = () => {
     messageP.classList.toggle("sad"); //use .add("sad") to JUST change color
 }
 
+//happy script
+document.getElementById("btn-happy").onclick = () => {
+    const displayP = document.getElementById("display");
+    displayP.classList.remove("hidden");
+    displayP.classList.add("happy");
+    displayP.innerHTML = "Good Times";
+}
+
+//sad script
+document.getElementById("btn-sad").onclick = () => {
+    const displayP = document.getElementById("display");
+    displayP.classList.remove("hidden");
+    displayP.classList.add("sad");
+    displayP.innerHTML = "Sad Times";
+}
+
+document.getElementById("btn-clear").onclick = () => {
+    document.getElementById("display").classList.add("hidden");
+}
+
+//key down
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    //const emotion = document.getElementById("txt-emotion").value;
+    document.getElementById("emotional-message").innerHTML = "You are feeling " + 
+    event.currentTarget.value;
+}
