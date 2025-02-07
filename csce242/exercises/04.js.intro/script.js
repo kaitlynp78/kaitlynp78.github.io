10a7a2677c386a34339e5b12242910b017a65997
kaitlynp78 <kspinkston05@gmail.com>
2025-02-06 23:45:41 -0500

    JS Intro

--- csce242/exercises/04.js.intro/script.js
+++ csce242/exercises/04.js.intro/script.js
@@ -1,14 +1,15 @@
 //do stuff when button clicked
 document.getElementById("btn-click-me").onclick = () => {
     console.log("WOW!");
-    document.getElementById("result").innerHTML = "Hi Kaitlyn";
-}
+    document.getElementById("result").innerHTML="Hi Kaitlyn";
+};
 
 //change color
 document.getElementById("btn-color").onclick = () => {
     const messageP = document.getElementById("message");
-    messageP.innerHTML = "Goodbye";
-    messageP.classList.toggle("sad"); //use .add("sad") to JUST change color
+    messageP.innerHTML = "Good bye";
+    messageP.classList.toggle("sad");
+
 }
 
 //happy script
@@ -17,23 +18,23 @@ document.getElementById("btn-happy").onclick = () => {
     displayP.classList.remove("hidden");
     displayP.classList.add("happy");
     displayP.innerHTML = "Good Times";
-}
+};
 
 //sad script
 document.getElementById("btn-sad").onclick = () => {
     const displayP = document.getElementById("display");
     displayP.classList.remove("hidden");
     displayP.classList.add("sad");
-    displayP.innerHTML = "Sad Times";
-}
+    displayP.innerHTML = "Sad TImes";
+};
 
 document.getElementById("btn-clear").onclick = () => {
     document.getElementById("display").classList.add("hidden");
-}
+};
 
 //key down
 document.getElementById("txt-emotion").onkeyup = (event) => {
     //const emotion = document.getElementById("txt-emotion").value;
-    document.getElementById("emotional-message").innerHTML = "You are feeling " + 
-    event.currentTarget.value;
+    document.getElementById("emotional-message").innerHTML = 
+    "You are feeling " + event.currentTarget.value;
 }
